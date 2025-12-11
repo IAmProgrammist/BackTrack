f36ee815b8bafb69c5b7433f886ba86b753cd976
IAmProgrammist <vladislav.pakhomov@bk.ru>
2025-12-11 21:20:55 +0300

    Authors completed endpoints

--- backend/app/api/v1/author.py
+++ backend/app/api/v1/author.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, Form
 from fastapi.responses import FileResponse
 from starlette.status import HTTP_200_OK, HTTP_201_CREATED
+from typing import Annotated
 
 from app.api.dependencies.auth import get_current_user_auth
 from app.api.dependencies.database import get_repository
@@ -11,6 +12,8 @@ from app.database.repositories.authors import AuthorsRepository
 from app.models.file import File
 from app.schemas.author import AuthorResponse
 from app.services.authors import AuthorsService
+from app.services.files import FileService
+from app.database.repositories.files import FilesRepository
 from app.utils import ERROR_RESPONSES, ServiceResult, handle_result
 from uuid import UUID
 from app.schemas.author import (
@@ -18,7 +21,9 @@ from app.schemas.author import (
     AuthorPagination,
     AuthorSort,
     AuthorResponse,
+    AuthorInCreate
 )
+from app.models.user import User
 from pydantic_filters.plugins.fastapi import FilterDepends, PaginationDepends, SortDepends
 
 router = APIRouter()
@@ -47,7 +52,7 @@ async def get_author(*,
     name="get_authors",
     response_model=AuthorResponse
 )
-async def get_author(*,
+async def get_authors(*,
              author_service: AuthorsService = Depends(get_service(AuthorsService)),
              authors_filters: AuthorFilter = FilterDepends(AuthorFilter),
              authors_pagination: AuthorPagination = PaginationDepends(AuthorPagination),
@@ -59,6 +64,64 @@ async def get_author(*,
         authors_pagination=authors_pagination,
         authors_sort=authors_sort,
         author_repo=author_repo
+    )
+
+    return await handle_result(response)
+
+
+@router.post(
+    "",
+    status_code=HTTP_200_OK,
+    responses=ERROR_RESPONSES,
+    name="create_author",
+    response_model=AuthorResponse
+)
+async def create_author(*,
+             author_service: AuthorsService = Depends(get_service(AuthorsService)),
+             author_in: Annotated[AuthorInCreate, Form(media_type="multipart/form-data")],
+             author_repo: AuthorsRepository = Depends(get_repository(AuthorsRepository)),
+             files_service: FileService = Depends(get_service(FileService)),
+             file_repository: FilesRepository = Depends(get_repository(FilesRepository)),
+             settings: AppSettings = Depends(get_app_settings),
+             token_user: User = Depends(get_current_user_auth()),
+    ) -> AuthorResponse:
+    response = await author_service.create_author(
+        author_in=author_in,
+        author_repo=author_repo,
+        files_service=files_service,
+        file_repository=file_repository,
+        settings=settings,
+        token_user=token_user
+    )
+
+    return await handle_result(response)
+
+
+@router.put(
+    "/{user_id}",
+    status_code=HTTP_200_OK,
+    responses=ERROR_RESPONSES,
+    name="update_author",
+    response_model=AuthorResponse
+)
+async def update_author(*,
+             author_service: AuthorsService = Depends(get_service(AuthorsService)),
+             author_in: Annotated[AuthorInCreate, Form(media_type="multipart/form-data")],
+             author_repo: AuthorsRepository = Depends(get_repository(AuthorsRepository)),
+             files_service: FileService = Depends(get_service(FileService)),
+             file_repository: FilesRepository = Depends(get_repository(FilesRepository)),
+             settings: AppSettings = Depends(get_app_settings),
+             token_user: User = Depends(get_current_user_auth()),
+             user_id: UUID
+    ) -> AuthorResponse:
+    response = await author_service.update_author(
+        author_in=author_in,
+        author_repo=author_repo,
+        files_service=files_service,
+        file_repository=file_repository,
+        settings=settings,
+        token_user=token_user,
+        user_id=user_id
     )
 
     return await handle_result(response)
